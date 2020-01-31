ee0c6d93f882b71ea6eeb631c449a9ac0be18b34
Clément Haëck <52320542+Descanonges@users.noreply.github.com>
2020-01-31 16:29:19 +0100

    Better keybingings
    Unbing magit key that was conflicting
    
    Make custom evil motions for scrolling

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -18,17 +18,23 @@
 ;;         :desc "run codecell & step" "C" #'elpy-shell-send-codecell-and-step
 ;;         :desc "run def & step" "D" #'elpy-shell-send-defun-and-step))
 
-(map! (:map override
+(map! :map magit-mode-map "M-n" nil)
+(map!
         "M-t" #'evil-window-right
         "M-n" #'evil-window-left
         "M-g" #'evil-window-up
-        "M-r" #'evil-window-down))
+        "M-r" #'evil-window-down)
 
 (setq scroll-step 5)
-(map! (:map override
-        "<M-up>" (lambda () (interactive) (evil-scroll-line-up scroll-step))
-        "<M-down>" (lambda () (interactive) (evil-scroll-line-down scroll-step))))
-
+(evil-define-motion scroll-n-lines-up (count)
+  "Scroll `scroll-step' up"
+  (evil-scroll-line-up scroll-step))
+(evil-define-motion scroll-n-lines-down (count)
+  "Scroll `scroll-step' down"
+  (evil-scroll-line-down scroll-step))
+(map!
+        "<M-up>" #'scroll-n-lines-up
+        "<M-down>" #'scroll-n-lines-down)
 
 (map! :n "M-l" ":m-2")
 (map! :n "M-a" ":m+")
@@ -60,6 +66,7 @@
 
 (setq! avy-keys '(?n ?r ?t ?d ?e ?a ?i ?u))
 
+(setq display-line-numbers-type 'relative)
 
 (setq mouse-wheel-progressive-speed nil)
 (setq mouse-wheel-scroll-amount
@@ -73,4 +80,3 @@
 (setq! python-shell-interpreter "ipython"
        python-shell-interpreter-args "console --simple-prompt"
        python-shell-prompt-detect-failure-warning nil)
-
