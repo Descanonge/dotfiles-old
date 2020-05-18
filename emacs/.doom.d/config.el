a87fae795f8fa87fa842c79edac39db783d41bbd
Clément Haëck <52320542+Descanonges@users.noreply.github.com>
2020-05-18 11:51:27 +0200

    Use custom project capture template
    make filename relative to current project root

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -148,25 +148,31 @@
   (setq thunderbird-program "/usr/bin/thunderbird")
 
   :config
+  (defun org-capture-project-relative () ""
+         (let ((file (car (projectile-make-relative-to-root (list (buffer-file-name)))))
+               (text (string-trim (org-current-line-string))))
+           (format "* TODO %%?\n[[file:%s::%s]]\n\n" file text)))
   (setq org-capture-templates
         '(("t" "Personal todo" entry
            (file+headline +org-capture-todo-file "Inbox")
            "* %?\n" :prepend t)
-
           ;; Will use {project-root}/{todo,notes,changelog}.org, unless a
           ;; {todo,notes,changelog}.org file is found in a parent directory.
           ;; Uses the basename from `+org-capture-todo-file',
           ;; `+org-capture-changelog-file' and `+org-capture-notes-file'.
           ("p" "Templates for projects")
+          ;; ("pt" "Project-local todo" entry  ; {project-root}/todo.org
+          ;;  (file+headline +org-capture-project-todo-file "General")
+          ;;  "* TODO %?\n%a\n\n" :prepend t)
           ("pt" "Project-local todo" entry  ; {project-root}/todo.org
-           (file+headline +org-capture-project-todo-file "Inbox")
-           "* TODO %?\n%i\n%a" :prepend t)
+           (file+headline +org-capture-project-todo-file "General")
+           (function org-capture-project-relative) :prepend t)
           ("pn" "Project-local notes" entry  ; {project-root}/notes.org
-           (file+headline +org-capture-project-notes-file "Inbox")
-           "* %U %?\n%i\n%a" :prepend t)
+           (file+headline +org-capture-project-notes-file "General")
+           "* %U %?\n%a\n\n" :prepend t)
           ("pc" "Project-local changelog" entry  ; {project-root}/changelog.org
            (file+headline +org-capture-project-changelog-file "Unreleased")
-           "* %U %?\n%i\n%a" :prepend t)
+           "* %U %?\n%a" :prepend t)
 
           ;; Will use {org-directory}/{+org-capture-projects-file} and store
           ;; these under {ProjectName}/{Tasks,Notes,Changelog} headings. They
@@ -175,17 +181,17 @@
           ("o" "Centralized templates for projects")
           ("ot" "Project todo" entry
            (function +org-capture-central-project-todo-file)
-           "* TODO %?\n %i\n %a"
+           "* TODO %?\n %i\n %a\n"
            :heading "Tasks"
            :prepend nil)
           ("on" "Project notes" entry
            (function +org-capture-central-project-notes-file)
-           "* %U %?\n %i\n %a"
+           "* %U %?\n %i\n %a\n"
            :heading "Notes"
            :prepend t)
           ("oc" "Project changelog" entry
            (function +org-capture-central-project-changelog-file)
-           "* %U %?\n %i\n %a"
+           "* %U %?\n %i\n %a\n"
            :heading "Changelog"
            :prepend t)))
 
